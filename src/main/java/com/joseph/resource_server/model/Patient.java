3fb964d98a7df7b46a27849bce567c13a96ce928
JosephShortt <g00419690@atu.ie>
2026-05-11 22:12:45 +0100

    Added patient entity

---
+++ src/main/java/com/joseph/resource_server/model/Patient.java
@@ -0,0 +1,84 @@
+package com.joseph.resource_server.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name="patients")
+public class Patient {
+
+    @Id
+    private UUID id;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+
+    @Column
+    private LocalDate dateOfBirth;
+
+    private String gender;
+    private String address;
+    private String city;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
