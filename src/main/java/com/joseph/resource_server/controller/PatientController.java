7de2a4db53406dca7fd174636367214b1be8ca59
JosephShortt <g00419690@atu.ie>
2026-05-11 22:41:39 +0100

    Added rest endpoint for searching patients by name

---
+++ src/main/java/com/joseph/resource_server/controller/PatientController.java
@@ -0,0 +1,28 @@
+package com.joseph.resource_server.controller;
+
+import com.joseph.resource_server.model.Patient;
+import com.joseph.resource_server.service.PatientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/patients")
+public class PatientController {
+    private final PatientService patientService;
+
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Patient>> searchPatients(@RequestParam String name){
+        List<Patient> patients = patientService.searchPatients(name);
+        return ResponseEntity.ok(patients);
+    }
+}
